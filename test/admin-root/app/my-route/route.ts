194ae1a44541e12a58c4a5b602c979d023f59a70
Hristiyan Dodov <h.dodov@gmail.com>
2024-08-19 19:37:42 +0300

    Add issue reproduction

--- test/admin-root/app/my-route/route.ts
+++ test/admin-root/app/my-route/route.ts
@@ -1,3 +1,12 @@
+import payload from 'payload'
+
+console.log(payload.db) // undefined
+
+// TypeError: Cannot read properties of undefined (reading 'initializing')
+payload.db.initializing.then(() => {
+  console.log('initialized')
+})
+
 export const GET = () => {
   return Response.json({
     hello: 'elliot',
